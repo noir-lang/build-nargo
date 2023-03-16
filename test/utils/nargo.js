ab23a29ef81ee0212f2efbbf0caa56db7b9e4dd3
Tom French <15848336+TomAFrench@users.noreply.github.com>
2023-03-16 21:19:10 +0000

    Update test/utils/nargo.js
    Co-authored-by: Blaine Bublitz <blaine@aztecprotocol.com>

--- test/utils/nargo.js
+++ test/utils/nargo.js
@@ -1,3 +1,3 @@
 import { default as path } from "node:path";
 
-export const NARGO_BIN = process.env.NARGO_PATH ? path.resolve(process.env.NARGO_PATH) : "nargo";
+export const NARGO_BIN = process.env.NARGO_BIN ? path.resolve(process.env.NARGO_BIN) : "nargo";
