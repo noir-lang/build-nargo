6d79c6683289d3cc667e63ff90430c7ccc4805c0
Tom French <tom@tomfren.ch>
2023-03-16 00:24:47 +0000

    fix: use `resolve` rather than `parse`

--- test/utils/nargo.js
+++ test/utils/nargo.js
@@ -1,3 +1,3 @@
-import { parse } from "node:path";
+import { default as path } from "node:path";
 
-export const NARGO_BIN = process.env.NARGO_PATH ? parse(process.env.NARGO_PATH) : "nargo";
+export const NARGO_BIN = process.env.NARGO_PATH ? path.resolve(process.env.NARGO_PATH) : "nargo";
